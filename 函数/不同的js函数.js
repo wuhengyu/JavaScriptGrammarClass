42b588d676e7a6984e3c11980123d8cb37c4cfb5
吴恒宇 <578060214@qq.com>
2023-03-20 18:27:24 +0800

    first commit

---
+++ 函数/不同的js函数.js
@@ -0,0 +1,17 @@
+// 无参数函数
+function myfunction(){
+  console.log("无参数函数");
+}
+myfunction();
+
+// 有参数函数
+function myfunction1(name1, name2){
+  console.log(name1+name2);
+}
+myfunction1("有参数函数1, ","有参数函数2");
+
+// 带返回值的函数
+function myfunction2(name1, name2){
+  return name1+name2;
+}
+myfunction2("带返回值的函数1, ","带返回值的函数2");
